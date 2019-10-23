250b2bc0921d5fded8405327c3e43b0a83205efd
PabloCorrea99 <38085662+PabloCorrea99@users.noreply.github.com>
2019-10-23 13:36:29 -0500

    Taller 3 -Persistencia

---
+++ api/server.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify, request 
+from flask_cors import CORS 
+from controllers.Predios import Predio
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/Predios',methods=['GET'])
+def getAll(): 
+    return (Predio.list())
+
+@app.route('/Predios',methods=['POST'])
+def postOne(): 
+    body = request.json 
+    return (Predio.create(body))
+
+app.run(port=5000, debug=True) 
